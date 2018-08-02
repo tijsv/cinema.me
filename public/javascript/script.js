05e957cde5da37d574c551c0763dd5146c50fa4c
tijsv <tijs.verellen@hotmail.com>
2018-08-02 22:30:46 +0200

    start api server side

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -11,10 +11,10 @@ $(document).ready(function(){
   $('#header-logo').on('click', function(){
     window.location.href = "/";
   });
-  // get movie when on the my_screen page
-  if(window.location.pathname === '/cinema/my_screen') {
-    getId();
-  }
+  // // get movie when on the screen page
+  // if(window.location.pathname === '/cinema/screen') {
+  //   getId();
+  // }
   // show movies when on
   if(window.location.pathname === '/cinema') {
     let allMovies = document.getElementsByClassName('movie');
@@ -31,7 +31,11 @@ $(document).ready(function(){
 function resultSelected(Id, type) {
   sessionStorage.setItem('Id', Id);
   sessionStorage.setItem('type', type);
-  window.location = 'cinema/my_screen';
+  if(window.location.href.indexOf("cinema/screen/") > -1) {
+    window.location = '/cinema/screen/'+type+Id;
+  } else {
+    window.location = 'cinema/screen/'+type+Id;
+  }
   return false;
 }
 
@@ -73,65 +77,65 @@ function searchTMDB(searchText){
   HTTPRequest.send();
 }
 
-function getId(){
-  console.log('getId started ...');
-  let Id = sessionStorage.getItem('Id');
-  let type = sessionStorage.getItem('type');
-  let url = 'https://api.themoviedb.org/3/';
-  if(type=="movie"){
-    url += 'movie/' + Id + '?api_key=' + api_key + '&language=en-US';
-  } else if(type=="tv"){
-    url += 'tv/' + Id + '?api_key=' + api_key + '&language=en-US';
-  }
-  var HTTPRequest = new XMLHttpRequest();
-  HTTPRequest.open('GET', url, true);
-  HTTPRequest.onload = function () {
-    var result = JSON.parse(this.response);
-    console.log(result);
-    var poster = 'https://image.tmdb.org/t/p/w500';
-    var title = "";
-    var released = "";
-    if(result.poster_path) {
-      poster += result.poster_path;
-    } else {
-      poster = "https://m.media-amazon.com/images/M/MV5BYThkZTZlZWQtNjMzMy00NTlkLThhOTQtMTNiZTc2MTBlNzExXkEyXkFqcGdeQXVyNzI1MDI1NDU@._V1_SX300.jpg"
-    }
-    if(type=="movie"){
-      title = result.title;
-      released = result.release_date;
-    } else if(type=="tv"){
-      title = result.name;
-      released = result.first_air_date;
-    }
-    var genreString = "";
-    for(i=0;i<result.genres.length;i++){
-      if(i==0){
-        genreString += result.genres[i].name;
-      } else{
-        genreString += ', ' + result.genres[i].name;
-      }
-    }
-    let output = `
-      <div class="my-screen">
-        <img src="${poster}">
-        <div class="text">
-          <p class="title">${title}</p>
-          <p class="genre">${genreString}</p>
-          <p class="plot">${result.overview}</p>
-          <p class="released"><span>Released:</span> ${released}</p>
-          <form method="POST" action="/cinema/my_screen">
-            <input type="hidden" name="Id" value="${Id}"/>
-            <input type="hidden" name="type" value="${type}"/>
-            <input class="submit" type="submit" value="Add to list"/>
-          </form>
-        </div>
-      </div>
-      <img class="background" src="${poster}">
-    `;
-    $('#movie').html(output);
-  }
-  HTTPRequest.send();
-}
+// function getId(){
+//   console.log('getId started ...');
+//   let Id = sessionStorage.getItem('Id');
+//   let type = sessionStorage.getItem('type');
+//   let url = 'https://api.themoviedb.org/3/';
+//   if(type=="movie"){
+//     url += 'movie/' + Id + '?api_key=' + api_key + '&language=en-US';
+//   } else if(type=="tv"){
+//     url += 'tv/' + Id + '?api_key=' + api_key + '&language=en-US';
+//   }
+//   var HTTPRequest = new XMLHttpRequest();
+//   HTTPRequest.open('GET', url, true);
+//   HTTPRequest.onload = function () {
+//     var result = JSON.parse(this.response);
+//     console.log(result);
+//     var poster = 'https://image.tmdb.org/t/p/w500';
+//     var title = "";
+//     var released = "";
+//     if(result.poster_path) {
+//       poster += result.poster_path;
+//     } else {
+//       poster = "https://m.media-amazon.com/images/M/MV5BYThkZTZlZWQtNjMzMy00NTlkLThhOTQtMTNiZTc2MTBlNzExXkEyXkFqcGdeQXVyNzI1MDI1NDU@._V1_SX300.jpg"
+//     }
+//     if(type=="movie"){
+//       title = result.title;
+//       released = result.release_date;
+//     } else if(type=="tv"){
+//       title = result.name;
+//       released = result.first_air_date;
+//     }
+//     var genreString = "";
+//     for(i=0;i<result.genres.length;i++){
+//       if(i==0){
+//         genreString += result.genres[i].name;
+//       } else{
+//         genreString += ', ' + result.genres[i].name;
+//       }
+//     }
+//     let output = `
+//       <div class="my-screen">
+//         <img src="${poster}">
+//         <div class="text">
+//           <p class="title">${title}</p>
+//           <p class="genre">${genreString}</p>
+//           <p class="plot">${result.overview}</p>
+//           <p class="released"><span>Released:</span> ${released}</p>
+//           <form method="POST" action="/cinema/screen">
+//             <input type="hidden" name="Id" value="${Id}"/>
+//             <input type="hidden" name="type" value="${type}"/>
+//             <input class="submit" type="submit" value="Add to list"/>
+//           </form>
+//         </div>
+//       </div>
+//       <img class="background" src="${poster}">
+//     `;
+//     $('#movie').html(output);
+//   }
+//   HTTPRequest.send();
+// }
 
 function getResultById(object, type){
   let Id = object.dataset.id;
--- routes/cinema.js
+++ routes/cinema.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+var api_key = "e13c2a215ac12da642ab41538b30e620";
 
 // user model
 let User = require('../models/user');
@@ -14,22 +16,70 @@ router.get('/', function(request, response) {
   }
 })
 
-router.get('/my_screen', function(request, response) {
+router.get('/screen/:typeid', function(request, response) {
   if(!request.user) {
     console.log('You have to log in first');
     request.flash('error', 'You have to log in first');
     response.redirect('/');
   } else {
-    response.render('./pages/my_screen.ejs');
+    let Id = request.params.typeid.match(/\d+/g).map(Number)[0].toString();
+    let type = request.params.typeid.replace(/[0-9]/g, '');
+    console.log(Id, type);
+    let url = 'https://api.themoviedb.org/3/';
+    if(type=="movie"){
+      url += 'movie/' + Id + '?api_key=' + api_key + '&language=en-US';
+    } else if(type=="tv"){
+      url += 'tv/' + Id + '?api_key=' + api_key + '&language=en-US';
+    }
+    axios.get(url)
+    .then(resp => {
+      let result = resp.data;
+      var poster = 'https://image.tmdb.org/t/p/w500';
+      var title = "";
+      var released = "";
+      if(result.poster_path) {
+        poster += result.poster_path;
+      } else {
+        poster = "https://m.media-amazon.com/images/M/MV5BYThkZTZlZWQtNjMzMy00NTlkLThhOTQtMTNiZTc2MTBlNzExXkEyXkFqcGdeQXVyNzI1MDI1NDU@._V1_SX300.jpg"
+      }
+      if(type=="movie"){
+        title = result.title;
+        released = result.release_date;
+      } else if(type=="tv"){
+        title = result.name;
+        released = result.first_air_date;
+      }
+      var genreString = "";
+      for(i=0;i<result.genres.length;i++){
+        if(i==0){
+          genreString += result.genres[i].name;
+        } else{
+          genreString += ', ' + result.genres[i].name;
+        }
+      }
+      var plot = result.overview;
+      response.render('./pages/screen.ejs', {
+        title: title,
+        poster: poster,
+        genreString: genreString,
+        released: released,
+        type: type,
+        plot: plot,
+        Id: Id
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 })
 
-router.post('/my_screen', function(request, response) {
+router.post('/screen/:typeid', function(request, response) {
   const Id = request.body.Id;
   const type = request.body.type;
   if(type != "movie" && type != "tv"){
     request.flash('error', 'You can only add a movie or a series to your list');
-    response.redirect('my_screen');
+    response.redirect('/cinema/screen/'+type+Id);
     return;
   } else {
     let user = request.user;
@@ -38,7 +88,7 @@ router.post('/my_screen', function(request, response) {
         user.cinema.movies.push(Id);
       } else {
         request.flash('error', 'I\'ll be back... This movie is already in your list');
-        response.redirect('my_screen');
+        response.redirect('/cinema/screen/'+type+Id);
         return;
       }
     } else if(type == "tv"){
@@ -46,7 +96,7 @@ router.post('/my_screen', function(request, response) {
         user.cinema.series.push(Id);
       } else {
         request.flash('error', 'I\'ll be back... This series is already in your list');
-        response.redirect('my_screen');
+        response.redirect('/cinema/screen/'+type+Id);
         return;
       }
     }
